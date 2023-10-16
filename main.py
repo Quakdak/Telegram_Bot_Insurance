9059e689f49e77ad365412e84d56541dc0d85764
Quakdak <quakdak100@mail.ru>
2023-10-16 14:10:28 +0300

    Added menu of user commands(will later update with creation of status commands)

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import asyncio
 
 from config.bot_config import dp, bot
 from handlers import register_user_commands
+from aiogram.types import BotCommand
+
+bot_commands = (
+    ("start", "Начало работы с ботом"),
+)
 
 
 async def main():
+    commands_for_bot = []
+    for cmd in bot_commands:
+        commands_for_bot.append(BotCommand(command=cmd[0], description=cmd[1]))
+    await bot.set_my_commands(commands=commands_for_bot)
+
     register_user_commands(dp)
     await dp.start_polling(bot)
 
