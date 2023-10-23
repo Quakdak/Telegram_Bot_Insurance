d3162de6f2752cc5892851ccc380462fb5f63f42
Quakdak <quakdak100@mail.ru>
2023-10-23 08:42:29 +0300

    Подготовка почвы

--- handlers/user_panel/request_for_transport/end_inspection_transport.py
+++ handlers/user_panel/request_for_transport/end_inspection_transport.py
@@ -1,16 +1,14 @@
-import os
-from dotenv import load_dotenv
 from aiogram.types import CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.context import FSMContext
 from lexicon.lexicon_ru import lexicon
+import utils.db_api.quick_commands as commands
 
 
 async def end_inspection_transport(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    load_dotenv()
-    user_id = callback.message.from_user.id
-    admin_ids = map(int, os.getenv('ADMIN_IDS').split())
+    user_id = data['user_id']
+    user = await commands.select_user(user_id)
 
     button_1 = InlineKeyboardButton(
         text='Подать заявку',
@@ -18,28 +16,34 @@ async def end_inspection_transport(callback: CallbackQuery, state: FSMContext):
     )
     button_2 = InlineKeyboardButton(
         text='Активные заявки',
-        callback_data='avtive_requests'
+        callback_data='active_requests'
     )
     button_3 = InlineKeyboardButton(
         text='Принятые заявки',
-        callback_data='applied_history'
+        callback_data='applied_requests'
     )
     inline_kb = [[button_1], [button_2], [button_3]]
-    if user_id in admin_ids:
+    if user.is_admin:
         button4 = InlineKeyboardButton(
             text='Админ панель',
-            callback_data='admin'
+            callback_data='admin_panel'
         )
         inline_kb.append([button4])
     keyboard = InlineKeyboardMarkup(
         inline_keyboard=inline_kb
     )
+    print(data.keys())
     if 'windshield' in data and 'key' in data and 'mark_windshield' in data and 'odometer' in data \
             and 'transport_inside' in data and 'transport_outside' in data and 'vin_number' in data \
             and 'wheel' in data:
+        user_id = data['user_id']
+        del data['current_keyboard']
+        del data['user_id']
+        await commands.add_vehicle_request(user_id, **data)
+        data = dict(user_id=user_id, current_keyboard=inline_kb)
         await callback.message.edit_text(text=lexicon['end_inspection'], reply_markup=keyboard)
-        await state.clear()
-    elif len(data) < 2:
+        await state.set_data(data)
+    elif len(data) < 3:
         await callback.message.edit_text(text=lexicon['early_end_inspection'], reply_markup=keyboard)
     else:
         keyboard = InlineKeyboardMarkup(
